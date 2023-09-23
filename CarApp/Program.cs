977208451daec4fd929b2ce8482039248f6da503
Orazbay <you@biy.kz>
2023-09-23 16:14:05 +0600

    Добавьте файлы проекта.

---
+++ CarApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CarApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[5];
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i] = new Car($"Brand {i + 1}", 2020 + i, 2.0 + i);
+                cars[i].Start();
+            }
+
+            foreach (var car in cars)
+            {
+                car.DisplayInformation();
+                Console.WriteLine();
+            }
+
+            double increment = 0.5;
+            cars[0].IncreaseEngineCapacity(ref increment);
+            Console.WriteLine($"Engine capacity of car 1 increased to {cars[0].EngineCapacity} liters");
+
+            Console.WriteLine($"Total number of cars: {Car.TotalCars}");
+        }
+    }
+}
---
+++ CarLibrary/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public partial class Car
+    {
+        private string brand;
+        private int yearManufactured;
+        private double engineCapacity;
+        private bool isRunning;
+        private static int totalCars;
+
+        public Car()
+        {
+            brand = "Unknown";
+            yearManufactured = DateTime.Now.Year;
+            engineCapacity = 2.0;
+            isRunning = false;
+            totalCars++;
+        }
+
+        public Car(string brand, int year, double engine)
+        {
+            this.brand = brand;
+            yearManufactured = year;
+            engineCapacity = engine;
+            isRunning = false;
+            totalCars++;
+        }
+
+        public string GetBrand()
+        {
+            return brand;
+        }
+
+        public void SetBrand(string newBrand)
+        {
+            brand = newBrand;
+        }
+
+        public int GetYearManufactured()
+        {
+            return yearManufactured;
+        }
+
+        public void SetYearManufactured(int newYear)
+        {
+            yearManufactured = newYear;
+        }
+
+        public void Start()
+        {
+            isRunning = true;
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+
+        public void IncreaseEngineCapacity(ref double increment)
+        {
+            engineCapacity += increment;
+        }
+
+        public static int TotalCars
+        {
+            get { return totalCars; }
+        }
+
+        public double EngineCapacity
+        {
+            get { return engineCapacity; }
+        }
+
+        static Car()
+        {
+            totalCars = 0;
+        }
+
+        public Car(string brand, int yearManufactured, double engineCapacity, bool isRunning) : this(brand, yearManufactured, engineCapacity)
+        {
+            this.isRunning = isRunning;
+        }
+    }
+    public partial class Car
+    {
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"Brand: {brand}");
+            Console.WriteLine($"Year Manufactured: {yearManufactured}");
+            Console.WriteLine($"Engine Capacity: {engineCapacity} liters");
+            Console.WriteLine($"Is Running: {(isRunning ? "Yes" : "No")}");
+        }
+    }
+}
